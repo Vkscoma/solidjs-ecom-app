5d1d74758c4f15dffb31e5b3b14b5386f9573e08
Vkscoma <vkscoma@gmail.com>
2023-12-27 07:50:35 -0800

    add base files

---
+++ custom-ecom-app/src/App.jsx
@@ -0,0 +1,19 @@
+import customBanner from './assets/customBanner.png';
+import Card from './components/card';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Merch</h1>
+      </header>
+      <img src={customBanner} alt="store banner" />
+
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ custom-ecom-app/src/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card(props) {
+    return (
+        <div>
+            <h2>Card Component</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis amet ullam, voluptates minus esse odit!</p>
+        </div>
+    )
+}
